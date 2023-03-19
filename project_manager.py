5ce2bf9f51d078d0797444d7afb5a375524252ac
GarborSergey <garborfersru@gmail.com>
2023-03-19 16:02:49 +0300

    START CREATE Project CLASS

--- project_manager.py
+++ project_manager.py
@@ -1,6 +1,17 @@
+from datetime import datetime
+
 
 class Project:
-    pass
+    def __init__(self, name: str, counterparty: str):
+        self.name = name
+        self.counterparty = counterparty
+        self.dateCreate = datetime.now()
+
+    def rename_project(self, new_name: str):
+        self.name = new_name
+
+    def rename_counterparty(self, new_name: str):
+        self.counterparty = new_name
 
 
 if __name__ == '__main__':
