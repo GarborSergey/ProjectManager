388d7277802f9e8051f8ae9553361c83cea14143
Сергей <garborfersru@gmail.com>
2023-03-24 17:20:14 +0300

    CONTINUE CONSTRUCT Project Class 1.Add .mkp files to marking 2.Write method to put project in work

--- project_manager.py
+++ project_manager.py
@@ -19,13 +19,12 @@ class ProjectManagerError(Exception):
 
 
 class Project:
-    def __init__(self, name, counterparty, path, templatePath):
+    def __init__(self, name, counterparty, comment=None):
         self.__name = self.convert_string(name)
         self.__counterparty = self.convert_string(counterparty)
         self.__dateCreate = datetime.now()
         self.__dateModification = datetime.now()
-
-        self.__create_project(path, templatePath)
+        self.__comment = comment
 
     @property
     def name(self):
@@ -54,7 +53,13 @@ class Project:
         return string.replace(' ', '_')
 
     # path is a global BASE_DIR that in config app
-    def __create_project(self, path, templatePath):
+    def create_project(self, path, templatePath):
+        """
+        Create empty folders fo projects and copy AutoCad dwg file
+        :param path: base dir for projects
+        :param templatePath: dir with files of templates
+        :return: None
+        """
         pathCounterparty = path + os.sep + self.__counterparty  # path to Counterparty dir
         pathProject = pathCounterparty + os.sep + self.__name  # path to Project dir
 
@@ -71,13 +76,71 @@ class Project:
 
         # create empty folder for source project raw
         os.mkdir(pathProject + os.sep + f'!исходный_проект_{self.__name}')
-        # copy and rename template files
+        os.mkdir(pathProject + os.sep + f'дополненеия_{self.__name}')
+        os.mkdir(pathProject + os.sep + f'печать_{self.__name}')
+        # copy and rename template file for AutoCad
         shutil.copy2(templatePath + os.sep + 'TEMPLATE.dwg', pathProject + os.sep + f'внешний_вид_{self.__name}.dwg')
 
+        self.__create_config_file(pathProject)
+        self.__create_comment_file(pathProject)
+
     # create INI file with all params of project
-    def __create_infoINI_file(self, path):
-        pass
+    def __create_config_file(self, path):
+        """
+        Create a config file
+        :param path: path to create config file
+        :return: None
+        """
+        config = configparser.ConfigParser()
+        config.add_section('ProjectSettings')
+        config.set('ProjectSettings', 'name', self.__name)
+        config.set('ProjectSettings', 'counterparty', self.__counterparty)
+        config.set('ProjectSettings', 'dateCreate', str(self.__dateCreate))
+        config.set('ProjectSettings', 'dateModification', str(self.__dateModification))
+
+        with open(path + os.sep + 'projectConfig.ini', 'w') as configFile:
+            config.write(configFile)
+
+    def __create_comment_file(self, path):
+        """
+        Create comment .txt file
+        :param path: path to create comment file
+        :return: None
+        """
+        with open(path + os.sep + f'comment_{self.__name}.txt', 'w') as commentFile:
+            commentFile.write(f'Project - {self.__name}\nCounterparty - {self.__counterparty}\n')
+            if self.__comment:
+                commentFile.write(self.__comment)
+
+    def put_the_project_into_work(self, path, templatePath):
+        """
+        Create files in project dir for work
+        :param path: base dir for projects
+        :param templatePath: dir with files of templates
+        :return: None
+        """
+        pathCounterparty = path + os.sep + self.__counterparty  # path to Counterparty dir
+        pathProject = pathCounterparty + os.sep + self.__name  # path to Project dir
+
+        # create empty folder for making files
+        os.mkdir(pathProject + os.sep + f'маркировка_{self.__name}')
+
+        # create empty folder for specification of boxes
+        os.mkdir(pathProject + os.sep + f'наборки_{self.__name}')
+
+        # create empty folder for passports
+        os.mkdir(pathProject + os.sep + f'паспорта_{self.__name}')
+
+        # copy all files in template_files/marking
+        for root, dirs, files in os.walk(templatePath + os.sep + 'marking'):
+            for fileName in files:
+                shutil.copy(templatePath + os.sep + 'marking' + os.sep + fileName, pathProject + os.sep + f'маркировка_{self.__name}')
+
+        # copy AutoCad file dwg for electrical diagrams
+        shutil.copy2(templatePath + os.sep + 'TEMPLATE.dwg', pathProject + os.sep + f'схемы_{self.__name}.dwg')
 
 
 if __name__ == '__main__':
-    p = Project('Test Project Name', 'Test Counterparty Name', BASE_DIR, TEMPLATE_DIR)
+    p = Project('Test Project Name1', 'Test Counterparty Name', 'Test comment test comment')
+    p.create_project(BASE_DIR, TEMPLATE_DIR)
+    p.put_the_project_into_work(BASE_DIR, TEMPLATE_DIR)
